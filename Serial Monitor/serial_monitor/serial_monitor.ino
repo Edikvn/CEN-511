e8ecef80d9b821dcaf12817102303e727a6d35c6
Edikvn <52725031+Edikvn@users.noreply.github.com>
2021-02-05 07:37:24 +0100

    CEN 511 Assignment
    Edikan Essienenkak 16CJ020721

---
+++ Serial Monitor/serial_monitor/serial_monitor.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello My name is Edikan Essienenkak, 16CJ020721");
+  delay(3000);
+}
